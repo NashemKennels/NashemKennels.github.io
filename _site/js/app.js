4b6544757c0022d1039ce513c97698c07b183e57
Ben Karl <benrkarl@gmail.com>
2014-10-18 09:37:23 -0400

    add Angus to the site

--- _site/js/app.js
+++ _site/js/app.js
@@ -1,6 +1,17 @@
 var nashemDogs = [
   {
-    name        : 'Taylor',
+    name        : '"Angus"',
+    kennelName  : 'He\'s the Man of Woodmoor CD NDD RN CGC',
+    credentials : ['Multiple BIS BISS CH'],
+    mainImg     : {img: '../images/Angus/Angus.JPG', caption: 'Angus, June 2008'},
+    sex         : 'male',
+    born        : 'May 16, 2006',
+    died        : undefined,
+    altImgs     : {},
+    pedigreeLink: '../docs/Angus-Ped.pdf'
+  },
+  {
+    name        : '"Taylor"',
     kennelName  : 'Taylor Maid CD NDD',
     credentials : ['All Breed BIS BISS CH', '14 Best in Shows', '2 BoB Westminster Kennel Club 1996, 1997 Group 3', 'BMDCA BoB 1997, 2008 National Specialty'],
     mainImg     : {img: '../images/Taylor/TaylorRibbons.jpg', caption: 'With her ribbons, December 04, 2005'},
