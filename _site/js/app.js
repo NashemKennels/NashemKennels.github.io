8d1503d4d5dbf6c2b99b300d231b7d383d8ad922
Ben Karl <benrkarl@gmail.com>
2014-10-18 11:28:19 -0400

    finish nashemDogs object and create initialize function for all dogs, need to update template with better styling

--- _site/js/app.js
+++ _site/js/app.js
@@ -6,9 +6,64 @@ var nashemDogs = [
     mainImg     : {img: '../images/Angus/Angus.JPG', caption: 'Angus, June 2008'},
     sex         : 'male',
     born        : 'May 16, 2006',
-    died        : undefined,
+    died        : false,
     altImgs     : {},
     pedigreeLink: '../docs/Angus-Ped.pdf'
+  },
+  {
+    name        : '"Rufio"',
+    kennelName  : 'Play It Again Sapphire',
+    credentials : ['BIS CH'],
+    mainImg     : {img: '../images/Rufio/Rufio_snow.jpg', caption: 'Rufio in the snow'},
+    sex         : 'male',
+    born        : 'September 22, 2004',
+    died        : false,
+    altImgs     : {},
+    pedigreeLink: '../docs/Rufio-Ped.pdf'
+  },
+  {
+    name        : '"Roxie"',
+    kennelName  : 'Party Like a Rock Star',
+    credentials : ['CH'],
+    mainImg     : {img: '../images/Roxie/Roxie.jpg', caption: 'Little Roxie'},
+    sex         : 'female',
+    born        : 'September 10, 2008',
+    died        : false,
+    altImgs     : {},
+    pedigreeLink: '' //need Roxie pedigree
+  },
+  {
+    name        : '"Ziva"',
+    kennelName  : 'Double Dare Ya',
+    credentials : ['CH'],
+    mainImg     : {img: '../images/Ziva/Ziva_02.JPG', caption: 'Ziva'},
+    sex         : 'female',
+    born        : '',
+    died        : false,
+    altImgs     : {},
+    pedigreeLink: 'PedigreeZiva.pdf'
+  },
+  {
+    name        : '"Annie"',
+    kennelName  : 'Dreamboat of Sapphire CD',
+    credentials : ['CH'],
+    mainImg     : {img: '../images/Annie/Annie-Scottsdale.jpg', caption: 'Annie in Scottsdale'},
+    sex         : 'female',
+    born        : 'December 12, 2002',
+    died        : false,
+    altImgs     : {},
+    pedigreeLink: '../docs/AnniePedigree.pdf'
+  },
+  {
+    name        : '"Harley"',
+    kennelName  : 'Swiss Star\'s Criminal Intent',
+    credentials : ['CH'],
+    mainImg     : {img: '../images/Harley/Harley_ears.jpg', caption: 'Harley hears something'},
+    sex         : 'female',
+    born        : 'May 23, 2005',
+    died        : false,
+    altImgs     : {},
+    pedigreeLink: '../docs/Harley-Ped.pdf'
   },
   {
     name        : '"Taylor"',
@@ -20,7 +75,106 @@ var nashemDogs = [
     died        : 'August 7, 2006',
     altImgs     : {},
     pedigreeLink: '../docs/Taylor-ped.pdf'
-  }
+  },
+  {
+    name        : '"Ricky"',
+    kennelName  : 'Got It Made CD NDD',
+    credentials : ['CH'],
+    mainImg     : {img: '../images/Ricky/Ricky.jpg', caption: 'Ricky in the mountains'},
+    sex         : 'male',
+    born        : 'December 1, 1999',
+    died        : 'November 27, 2010',
+    altImgs     : {},
+    pedigreeLink: '../docs/Ricky-ped.pdf'
+  },
+  {
+    name        : '"Vanna"',
+    kennelName  : 'Tailwind Moondance',
+    credentials : ['CH'],
+    mainImg     : {img: '../images/Vanna/HelenVanna1a.jpg', caption: 'Vanna with Helen'},
+    sex         : 'female',
+    born        : 'January 9, 1999',
+    died        : 'November 10, 2009',
+    altImgs     : {},
+    pedigreeLink: false
+  },
+  {
+    name        : '"Austin"',
+    kennelName  : 'Bad to the Bone CD NDD',
+    credentials : ['CH'],
+    mainImg     : {img: '../images/Austin/Austin0001.jpg', caption: 'Austin'},
+    sex         : 'male',
+    born        : 'June 12, 1995',
+    died        : 'April 7, 2007',
+    altImgs     : {},
+    pedigreeLink: 'Austin-ped.pdf'
+  },
+  {
+    name        : '"Teazer"',
+    kennelName  : 'Rumple Teazer',
+    credentials : ['CH'],
+    mainImg     : {img: '../images/Teazer/Teazer.jpg', caption: 'Teazer'},
+    sex         : 'female',
+    born        : 'May 23, 1999',
+    died        : 'August 25, 2006',
+    altImgs     : {},
+    pedigreeLink: 'Teazer-ped.pdf'
+  },
+  {
+    name        : '"Gala"',
+    kennelName  : 'Gay Deciever CD NDD',
+    credentials : ['CH'],
+    mainImg     : {img: '../images/Gala/gayla.jpg', caption: 'Gala'},
+    sex         : 'female',
+    born        : 'August 28, 1992',
+    died        : 'May 7, 2000',
+    altImgs     : {},
+    pedigreeLink: false
+  },
+  {
+    name        : '"Becken"',
+    kennelName  : 'Nashem\'s Becken V Woodmoore CD',
+    credentials : ['CH'],
+    mainImg     : undefined,
+    sex         : 'female',
+    born        : 'December 13, 1989',
+    died        : 'January 13, 1995',
+    altImgs     : {},
+    pedigreeLink: false
+  },
+  {
+    name        : '"Dottie"',
+    kennelName  : 'Nashem\'s Jennany Dots',
+    credentials : ['CH'],
+    mainImg     : undefined,
+    sex         : 'female',
+    born        : 'May 3, 1999',
+    died        : false,
+    altImgs     : {},
+    pedigreeLink: false
+  },
+  {
+    name        : '"Strider"',
+    kennelName  : 'Nashem\'s Alpine Strider',
+    credentials : ['CH'],
+    mainImg     : undefined,
+    sex         : 'male',
+    born        : 'January 16, 1988',
+    died        : 'May 10, 1994',
+    altImgs     : {},
+    pedigreeLink: false
+  },
+  {
+    name        : '"Caty"',
+    kennelName  : 'Mijay\'s Calamity Jane',
+    credentials : [],
+    mainImg     : undefined,
+    sex         : 'female',
+    born        : 'November 8, 1985',
+    died        : 'April 3, 1989',
+    altImgs     : {},
+    pedigreeLink: false
+  },
 ]
 
 var App = function(){
@@ -38,12 +192,27 @@ var App = function(){
     dogCollection = new DogCollection(); //creates empty dog collection
 
     _.each(nashemDogs, function(dog){ //populates collection with models created from object above
-      var dogModel = new Dog(dog);
-      dogCollection.add(dogModel);
+      if (!dog.died){
+        var dogModel = new Dog(dog);
+        dogCollection.add(dogModel);
+      }
     });
 
     dogListView = new DogListView({collection: dogCollection, el: $('.dog-container') });
-  }
+  };
+
+  var initializeLostDogs = function(){
+      dogCollection = new DogCollection(); //creates empty dog collection
+
+      _.each(nashemDogs, function(dog){ //populates collection with models created from object above
+        if (dog.died){
+          var dogModel = new Dog(dog);
+          dogCollection.add(dogModel);
+        }
+      });
+
+      dogListView = new DogListView({collection: dogCollection, el: $('.dog-container') });
+  };
 
 };
 
